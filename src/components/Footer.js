708f41078a2aa03febe174cab51d7b8687de6272
山下優太郎 <yamashitayuutarou@yamashirounoMBP.lan>
2023-11-12 11:46:56 +0900

    Initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2023 My App</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/MainContent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './MainContent.css';
+
+function MainContent() {
+  return (
+    <div className="main-content">
+      <img src="/images/myImage.jpg" alt="Description" />
+      <p>This is the main area of the app. Add your content here.</p>
+    </div>
+  );
+}
+
+export default MainContent;
